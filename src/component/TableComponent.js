1b822dbedf652cddd4e85c51eec8a573fed09be5
wijaya014 <wijaya.agus014@gmail.com>
2021-07-22 19:10:28 +0700

    add bootstrap lib add component Navbar, Table, Form

---
+++ src/component/TableComponent.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+
+const TableComponent = () => {
+  return (
+    <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+};
+
+export default TableComponent;
